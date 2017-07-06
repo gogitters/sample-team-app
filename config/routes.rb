8e8adfa43051bed1a4a836e9b52f538684606f7b
Jordan Hiatt <jordan.hiatt11@gmail.com>
2017-07-06 18:32:18 -0400

    add jordans controller, to control the jordans

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,6 @@ Rails.application.routes.draw do
   get "/pages", to: 'pages#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  get "/jordans/", to: 'jordans#index'
 end
