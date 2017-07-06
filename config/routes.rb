098d9a80f0beac4333359347e73a798e4cf597ae
David Astor <toodimes@gmail.com>
2017-07-06 18:58:40 -0400

    astor controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  get 'astors/index'
 
   root to: 'pages#index'
   get "/pages", to: 'pages#index'
 
+  get "/astors" => 'astors#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
