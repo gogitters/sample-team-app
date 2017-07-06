4d3c8ab473c1d753dce2b839ca1fd92793aade6e
Samantha Morco <samanthamorco@gmail.com>
2017-07-06 18:42:18 -0400

    create chips controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+
   root to: 'pages#index'
   get "/pages", to: 'pages#index'
+  get "/chips", to: 'chips#index'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
