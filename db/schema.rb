164739c31e7550780fa2085cfe4382387f381438
Samantha Morco <samanthamorco@gmail.com>
2017-07-06 20:04:53 -0400

    create person model

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170706230146) do
+ActiveRecord::Schema.define(version: 20170707000443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "takumis", force: :cascade do |t|
     t.string "gyoza"
     t.string "ramen"
     t.string "bobatea"
     t.boolean "hungry"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
