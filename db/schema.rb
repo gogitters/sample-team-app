aaf52f819c7ac4c1b3bcb15c8bd8ac9a4f9ada86
Samantha Morco <samanthamorco@gmail.com>
2017-07-06 20:04:53 -0400

    create person model with name and email attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170707000208) do
+ActiveRecord::Schema.define(version: 20170707000443) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "people", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "email"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "takumis", force: :cascade do |t|
     t.string "gyoza"
